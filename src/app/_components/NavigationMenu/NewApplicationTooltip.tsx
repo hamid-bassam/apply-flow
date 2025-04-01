0d1193cb19c8f4df50e5bb798c6ecdf761508006
hamid-bassam <hamid.bassam.147@gmail.com>
2025-04-01 19:45:30 +0200

    route application

--- src/app/_components/NavigationMenu/NewApplicationTooltip.tsx
+++ src/app/_components/NavigationMenu/NewApplicationTooltip.tsx
@@ -7,7 +7,7 @@ export const NewApplicationTooltip = () => {
     <Tooltip>
       <TooltipTrigger asChild>
         <Link
-          href="/dashboard/new-application"
+          href="/dashboard/application"
           className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-primary  md:h-8 md:w-8"
         >
           <FilePlus className="h-5 w-5" />
