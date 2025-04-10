9c8afd0c5fe71580f1ac3be8c95e6c5cca79b9eb
hamid-bassam <hamid.bassam.147@gmail.com>
2025-04-10 05:37:57 +0200

    border fix

--- src/app/_components/Header/AppHeader.tsx
+++ src/app/_components/Header/AppHeader.tsx
@@ -6,7 +6,7 @@ import { BurgerMenu } from "./BurgerMenu";
 import { SearchBar } from "./SearchBar";
 export const AppHeader = () => {
   return (
-    <header className="sticky top-0 z-30  flex h-14 items-center gap-4 bg-background px-4 sm:static sm:h-auto  sm:bg-transparent sm:px-6 py-2 border boder-b-2">
+    <header className="sticky top-0 z-30  flex h-14 items-center gap-4 bg-background px-4 sm:static sm:h-auto  sm:bg-transparent sm:px-6 py-2 border border-border boder-b-2">
       {/* border-b  sm:border-0*/}
       <BurgerMenu />
       <BreadCrumbNavigation />
